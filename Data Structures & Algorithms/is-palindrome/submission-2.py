7e48cb9410db5c2c112baa1276b348d1143b2612
Lee, Chiao-Yu <chiaoyu.lee7280@gmail.com>
2026-04-26 15:48:42 +0800

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join(c for c in s if c.isalnum()).lower()
+        if s == s[::-1]:
+            return True
+        else:
+            return False
